3ac1b68e0ea6d2fe25a3132d85304173a9411c06
gabrildnsilva <gabrieldnsilva@gmail.com>
2025-05-16 17:22:05 -0300

    feat: first prototype of [Card] & [App] in App.Jsx - Fixed package-lock.json name

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-const App () => {
+const Card = ( {title}) => {
   return (
-    <h2>Funtional Arrow Component </h2>
+    <div style={{
+      border: '1px solid #4b5362',
+      padding: '20px',
+      margin: '10px',
+      backgroundColor: '#31363f',
+      
+    }}>  
+      <h2>{title}</h2>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className='card-container'>
+      <Card title="Movie 1" />
+      <Card title="Movie 2" />
+      <Card title="Movie 3" />
+    </div>
   )
 }
 
