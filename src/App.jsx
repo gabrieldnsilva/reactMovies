6cf0262a4e1cfa5d081fa7684fb2f07dbbd55fa2
gabrildnsilva <gabrieldnsilva@gmail.com>
2025-06-22 12:46:20 -0300

    feat: implemented hasLiked useState()

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,59 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-const Card = ( {title}) => {
-  return (
-    <div className='card'>  
-      <h2>{title}</h2>
-    </div>
-  )
-}
+const Card = ({ title }) => {
+	const [hasLiked, setHasLiked] = useState(false);
+
+	return (
+		<div className="card">
+			<h2>{title}</h2>
+
+			<button
+				onClick={() => {
+					setHasLiked(!hasLiked);
+				}}
+			>
+				{hasLiked ? "❤️" : "🤍"}
+			</button>
+		</div>
+	);
+};
 
 const App = () => {
-  return (
-    <div className='card-container'>
-      <Card title="Star Wars" rating={5} isCool={true} actors={[{name: 'Mark Hamill'}, {name: 'Harrison Ford'}, {name: 'Carrie Fisher'}]} />
-      <Card title="Avatar" rating={4} isCool={true} actors={[{name: 'Sam Worthington'}, {name: 'Zoe Saldana'}, {name: 'Sigourney Weaver'}]} />
-      <Card title="The Lion King" rating={5} isCool={true} actors={[{name: 'Matthew Broderick'}, {name: 'James Earl Jones'}, {name: 'Jeremy Irons'}]} />
-    </div>
-  )
-}
+	return (
+		<div className="card-container">
+			<Card
+				title="Star Wars"
+				rating={5}
+				isCool={true}
+				actors={[
+					{ name: "Mark Hamill" },
+					{ name: "Harrison Ford" },
+					{ name: "Carrie Fisher" },
+				]}
+			/>
+			<Card
+				title="Avatar"
+				rating={4}
+				isCool={true}
+				actors={[
+					{ name: "Sam Worthington" },
+					{ name: "Zoe Saldana" },
+					{ name: "Sigourney Weaver" },
+				]}
+			/>
+			<Card
+				title="The Lion King"
+				rating={5}
+				isCool={true}
+				actors={[
+					{ name: "Matthew Broderick" },
+					{ name: "James Earl Jones" },
+					{ name: "Jeremy Irons" },
+				]}
+			/>
+		</div>
+	);
+};
 
-
-
-export default App
+export default App;
